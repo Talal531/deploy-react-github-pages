72d050d91d2c7b6bcb254d40b3474eb8fff21f13
Talal Malick <m.talalmalick2014@gmail.com>
2019-10-19 14:28:12 +0500

    Updates

---
+++ precache-manifest.5e836083cbde015b2cb128df63d31980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45939583b8339828411b25842d86f25",
+    "url": "/deploy-react-github-pages/index.html"
+  },
+  {
+    "revision": "813855ab27af79d354f9",
+    "url": "/deploy-react-github-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f337614954621c5dfa64",
+    "url": "/deploy-react-github-pages/static/js/2.fadc97bc.chunk.js"
+  },
+  {
+    "revision": "813855ab27af79d354f9",
+    "url": "/deploy-react-github-pages/static/js/main.36599bed.chunk.js"
+  },
+  {
+    "revision": "10a9386c03b9744ab191",
+    "url": "/deploy-react-github-pages/static/js/runtime-main.9e7a5d9c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/deploy-react-github-pages/static/media/logo.25bf045c.svg"
+  }
+]);
